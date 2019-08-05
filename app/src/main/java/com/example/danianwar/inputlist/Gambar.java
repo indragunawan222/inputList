e72eb3764ebec7a91d6f94b92da19e819849de2a
indragunawan222 <ig225970@gmail.com>
2019-08-05 09:45:06 +0700

    pertemuan input&listview

---
+++ app/src/main/java/com/example/danianwar/inputlist/Gambar.java
@@ -0,0 +1,19 @@
+package app.android.inputList;
+
+public class Gambar {
+    int image;
+    String nama;
+
+    public Gambar(int image, String nama) {
+        this.image = image;
+        this.nama = nama;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
